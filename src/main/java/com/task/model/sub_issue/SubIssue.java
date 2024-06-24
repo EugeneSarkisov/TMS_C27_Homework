a8cfcc447d29fb267e9de5121e0d223d82b782e9
EugeneSarkisov <154362125+EugeneSarkisov@users.noreply.github.com>
2024-06-24 18:18:04 +0300

    Update SubIssue.java

--- src/main/java/com/task/model/sub_issue/SubIssue.java
+++ src/main/java/com/task/model/sub_issue/SubIssue.java
@@ -5,11 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 public class SubIssue {
     private String subIssueDescription;
 
-    @Autowired
     public SubIssue() {
     }
 
-    @Autowired
     public SubIssue(String subIssueDescription) {
         this.subIssueDescription = subIssueDescription;
     }
